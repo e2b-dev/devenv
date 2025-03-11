108fec2f4adc6710cab982626373d1a03460fcc3
r33drichards <rwendt1337@gmail.com>
2025-03-10 23:43:09 -0700

    add gcc and clone devenv

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 
 # install and configure nix
 su ${username} -c "curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm"
-su ${username} -c "source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh && NIXPKGS_ALLOW_UNFREE=1 nix profile install --impure nixpkgs#git nixpkgs#go nixpkgs#pnpm_9 nixpkgs#python3 nixpkgs#nodejs nixpkgs#opentofu nixpkgs#google-cloud-sdk nixpkgs#tailscale nixpkgs#git-credential-oauth"
+su ${username} -c "source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh && NIXPKGS_ALLOW_UNFREE=1 nix profile install --impure nixpkgs#git nixpkgs#go nixpkgs#pnpm_9 nixpkgs#python3 nixpkgs#nodejs nixpkgs#opentofu nixpkgs#google-cloud-sdk nixpkgs#tailscale nixpkgs#git-credential-oauth nixpkgs#gcc"
 # so you can push to git repos without having to add a ssh key or use a PAT
 su ${username} -c "source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh && git credential-oauth configure"
 
@@ -11,4 +11,4 @@ su ${username} -c "source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon
 # one is we don't have to worry about having to authenticate to pull down the code :) 
 su ${username} -c "source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh && git clone https://github.com/e2b-dev/infra /home/${username}/infra"
 su ${username} -c "source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh && git clone https://github.com/e2b-dev/E2B /home/${username}/E2B"
-# su e2b -c "git clone https://github.com/e2b-dev/devnenv"
+su ${username} -c "source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh && git clone https://github.com/e2b-dev/devenv /home/${username}/devenv"
